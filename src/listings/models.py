28b53d6971894f073f589056c76161e211085e2e
Nido89 <syednaveedjalal@gmail.com>
2020-01-29 11:28:51 +0200

    new models updated

--- src/listings/models.py
+++ src/listings/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Listings(models.Model):
-	title       = models.CharField(max_length=200) #max_length=required intiger
-	description = models.TextField()
-	price       = models.TextField()
-	summary     = models.TextField(default='this is cool!')
+	title       = models.CharField(max_length=200) # max_length=required intiger
+	description = models.TextField(blank=True, null=True)
+	price       = models.DecimalField(decimal_places=2, max_digits=1000)
+	summary     = models.TextField()
 
 
 	
