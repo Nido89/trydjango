e2dfb71cde35e87f90d94a76f84eabe541c483f5
Nido89 <syednaveedjalal@gmail.com>
2020-02-01 20:35:24 +0200

    http url and routes

--- src/listings/models.py
+++ src/listings/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Listings(models.Model):
 	title       = models.CharField(max_length=200) # max_length=required intiger
 	description = models.TextField(blank=True, null=True)
-	price       = models.DecimalField(decimal_places=2, max_digits=1000)
-	summary     = models.TextField()
+	price       = models.DecimalField(decimal_places=2, max_digits=1000000)
+	summary     = models.TextField(blank=False, null=False)
+	featured    = models.BooleanField() #nulle = True, Default = True
 
 
 	
--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 
     #myownapps
+    'pages',
     'listings',
 ]
 
